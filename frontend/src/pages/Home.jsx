f85e3f02affce2fffa87f6d18694872f7d372ab8
Taz Larson <tazlarson1@gmail.com>
2024-01-08 14:35:14 -0800

    set up

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <h1>Beaver Brains</h1>
+            <p>Welcome to Beaver Brains!</p>
+        </div>
+    )
+}
