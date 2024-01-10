64e49461f59cfd477646d51876b0361ff4b7f533
Taz Larson <tazlarson1@gmail.com>
2024-01-09 17:57:31 -0800

    finsihing users auth

--- frontend/src/pages/components/Navbars/LandingNav.jsx
+++ frontend/src/pages/components/Navbars/LandingNav.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
 
 
 export default function LandingNav() {
@@ -7,7 +8,14 @@ export default function LandingNav() {
     <div className="sticky top-0 z-50">
       <div className="flex w-full h-16 bg-primary glass items-center justify-between">
         <h1 className="ml-5 text-tertiary">Beaver Brains</h1>
-        <Link to="/login" >Sign In</Link>
+
+        <div className="mr-5"><SignedOut><Link to="/login" className="mr-5">Sign In</Link></SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn></div>
+        
+        
+        {/* <SignInButton></SignInButton> */}
       </div>
     </div>
   )
